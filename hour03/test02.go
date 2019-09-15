5a98b72ba89cf035968bc907d32fb13940642ca1
Administrator <254195430@qq.com>
2019-09-15 10:55:25 +0800

    first commit

--- hour03/test02.go
+++ hour03/test02.go
@@ -1,25 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	//1.go语言切片是对原数组的映射，并没有创建一个真正的切片
-	// 定义数组
-	arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
-	//取切片
-	s1 := arr[2:]
-	//修改值
-	s1[0] = 100
-	fmt.Println(s1)
-	fmt.Println(arr)
-	fmt.Println()
-
-	// go语言切片没有取到的位置，可以向后延申，不可向前延申
-	s3 := arr[2:6]
-	fmt.Println(s3)
-	s4 := s3[3:5]
-	fmt.Println(s4)
-
-	//容量和大小
-	fmt.Printf("s3=%v,len(s3)=%d,cap(s3)=%d\n", s3, len(s3), cap(s3))
+	s := "Oh I do like to beside the seaside"
+	fmt.Println(strings.ToUpper(s))
+	s1 := strings.Replace(s, "beside", "haha", -1)
+	fmt.Println(strings.ToUpper(s1))
+	s2 := strings.Index(s, "the")
+	fmt.Println(s2)
 }
