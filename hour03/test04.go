0210e34f51339759e835b7e36e906e037541f7cb
Administrator <254195430@qq.com>
2019-09-18 21:53:48 +0800

    first commit

--- hour03/test04.go
+++ hour03/test04.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func Pinger(c chan string) {
+	t := time.NewTicker(1 * time.Second)
+	for {
+		c <- "ping"
+		<-t.C
+
+	}
+
+}
+
+func main() {
+	message := make(chan string)
+	go Pinger(message)
+	for i := 1; i <= 4; i++ {
+		msg := <-message
+		fmt.Println(msg)
+		if i == 3 {
+			fmt.Println("第三秒")
+			break
+		}
+	}
+}
